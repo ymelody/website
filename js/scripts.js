9f4f67d8e867ad86871d8221993373836d8c33a9
Hex <hex20dec@gmail.com>
2021-01-12 02:33:51 -0500

    upload all

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+    * Start Bootstrap - Grayscale v6.0.3 (https://startbootstrap.com/theme/grayscale)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    $("#contact-form").on("submit", (e)=>{
+        e.preventDefault();
+
+        let ajaxObj = {};
+        let $form = $(e.target);
+        var postURL = $form.attr("action");
+        $form.find("input, select, textarea").each(function(val) {
+            var name = $(this).attr("name");
+            if (name !== undefined && name !== "" &&
+                name !== null) {
+                ajaxObj[name] = $(this).val();
+            }
+        });
+        $.post(postURL, ajaxObj, function(){ 
+            let $successMsg = $("#success-msg");
+            $successMsg.show("fast", ()=>{
+                setTimeout(()=>{
+                    $successMsg.hide("fast");
+                },10000)
+            });
+            $form[0].reset();
+        });
+
+        return false;
+    });
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 70,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 100,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
